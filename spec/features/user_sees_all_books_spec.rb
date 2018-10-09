8d6b633bec13a0c7c26a4754de8be09376893df2
Bailey Diveley <bailey.diveley@gmail.com>
2018-10-09 16:35:07 -0600

    Make index list book titles and test

--- spec/features/user_sees_all_books_spec.rb
+++ spec/features/user_sees_all_books_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
-describe '' do
-  it '' do
+describe 'book index' do
+  it 'user sees all books' do
+    book_1 = Book.create(title: "Huckleberry Finn", pages: 250, year: 1950)
+    author = book_1.authors.create(name: "Mark Twain")
+
+    visit '/books'
+
+    expect(page).to have_content(book_1.title)
+    expect(page).to have_content(book_1.pages)
+    expect(page).to have_content(book_1.year)
+    expect(page).to have_content(author.name)    
   end
 end
