c9ebcc6a313db811e0a98b37036da968f0248e53
Bailey Diveley <bailey.diveley@gmail.com>
2018-10-09 17:17:31 -0600

    Make index display average book rating and total review count

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,8 @@ class Book < ApplicationRecord
 
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  def average_rating
+    reviews.average(:rating)
+  end
 end
--- spec/features/user_sees_all_books_spec.rb
+++ spec/features/user_sees_all_books_spec.rb
@@ -1,15 +1,26 @@
 require 'rails_helper'
 
 describe 'book index' do
-  it 'user sees all books' do
+  before(:each) do
     book_1 = Book.create(title: "Huckleberry Finn", pages: 250, year: 1950)
     author = book_1.authors.create(name: "Mark Twain")
-
+    user = User.create(name: "Joe Shmoe")
+    book_1.reviews.create(title: "Awesome book!", description: "This book is totally awesome", rating: 5, user_id: user.id)
+    book_1.reviews.create(title: "Crappiest book", description: "This book is terrible", rating: 1, user_id: user.id)
+    @books = [book_1]
+  end
+  it 'user sees all books' do
     visit '/books'
 
-    expect(page).to have_content(book_1.title)
-    expect(page).to have_content(book_1.pages)
-    expect(page).to have_content(book_1.year)
-    expect(page).to have_content(author.name)    
+    expect(page).to have_content(@books[0].title)
+    expect(page).to have_content(@books[0].pages)
+    expect(page).to have_content(@books[0].year)
+    expect(page).to have_content(@books[0].authors[0].name)
+  end
+  it 'user sees rating statistics per book' do
+    visit '/books'
+
+    expect(page).to have_content("Average Rating: 3")
+    expect(page).to have_content("Total Reviews: 2")
   end
 end
