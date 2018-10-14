0609014d5561c428e7fb0573db2627384164ae2e
William Fischer <william.fischer42@gmail.com>
2018-10-14 17:36:50 -0600

    Removed more commented out code.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,5 @@ class AuthorsController < ApplicationController
   def destroy
     Book.destroy_books_with_single_author(params[:id])
     Author.find(params[:id]).destroy
-    #
-    # where(author_id: author_id).destroy
-    # books_to_delete.destroy
-    # redirect_to(authors_path())
   end
 end
