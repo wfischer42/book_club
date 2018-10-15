09b1bb7ca75d50e1b957aaa2fe364d1ccde151c9
Bailey Diveley <41691872+bdiveley@users.noreply.github.com>
2018-10-15 15:37:35 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'books#index'
 
-  resources :books do
+  resources :books, only: [:create, :new, :index, :destroy, :show] do
     resources :reviews, only: [:new, :create]
   end
 
