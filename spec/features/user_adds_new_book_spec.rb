2f0048a91d4ece7540c461298663f4225f12a65e
Bailey Diveley <bailey.diveley@gmail.com>
2018-10-17 08:02:04 -0600

    Add final test for new book creation

--- spec/features/user_adds_new_book_spec.rb
+++ spec/features/user_adds_new_book_spec.rb
@@ -39,4 +39,14 @@ describe 'new book page' do
 
     expect(page).to have_current_path(book_path(book))
   end
+  it 'cannot be created if the all book info is not inputted' do
+    visit new_book_path
+
+    fill_in("book[title]", with: "Moby Dick")
+    fill_in("book[authors]", with: "ernest hemmingway, ghost writer")
+    fill_in("book[year]", with: "1934")
+    click_button("Create Book")
+
+    expect(page).to have_current_path(new_book_path)
+  end
 end
